693ec3e2668819e1f0eb1922ba984582089bfd12
Dude <dudeblogger1002gmail.com>
2024-06-25 12:52:29 +0530

    App Completed

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,23 +1,29 @@
 import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utils";
 
 const FavoritesPage = () => {
-  const fav = true;
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
   return (
     <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen ">
       <div className="max-w-screen-lg mx-auto">
         <h1 className="font-bold text-3xl md:text-5xl my-4">My Favorites</h1>
 
-        {!fav && (
+        {favorites.length === 0 && (
           <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="/404.svg" alt="404 page" className="h-3/4" />
           </div>
         )}
 
-        {fav && (
-          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <RecipeCard />
-          </div>
-        )}
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {favorites.map((recipe) => (
+            <RecipeCard
+              key={recipe.label}
+              recipe={recipe}
+              {...getRandomColor()}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,46 @@
 import { Search } from "lucide-react";
 import RecipeCard from "../components/RecipeCard";
+import { useEffect, useState } from "react";
+import { getRandomColor } from "../lib/utils";
+
+const APP_ID = import.meta.env.VITE_APP_ID;
+const APP_KEY = import.meta.env.VITE_APP_KEY;
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipe = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`
+      );
+      const data = await res.json();
+      // console.log(data.hits);
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe("chicken");
+  }, []);
+
+  const handleSearchRecipe = (e) => {
+    e.preventDefault();
+    fetchRecipe(e.target[0].value);
+  };
+
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
-        <form>
+        <form onSubmit={handleSearchRecipe}>
           <label className="input shadow-md flex items-center gap-2">
             <Search size={24} />
             <input
@@ -24,11 +59,24 @@ const HomePage = () => {
         </p>
 
         <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
           {/* 1st recipe  */}
+
+          {!loading &&
+            recipes.map(({ recipe }, index) => (
+              <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+            ))}
+
+          {loading &&
+            [...Array(9)].map((_, index) => (
+              <div key={index} className="flex flex-col gap-4 w-full">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="flex justify-between">
+                  <div className="skeleton h-4 w-28"></div>
+                  <div className="skeleton h-4 w-24"></div>
+                </div>
+                <div className="skeleton h-4 w-1/2"></div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
